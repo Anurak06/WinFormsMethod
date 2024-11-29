18ccd915977fd503484447c05c584dd22f512801
Anurak06 <0942713622ball@gmail.com>
2024-11-29 21:57:41 +0700

    Add project files.

---
+++ WinFormsMethod/Form1.cs
@@ -0,0 +1,114 @@
+﻿namespace WinFormsMethod
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("ยินดีต้อนรับ", "โปรแกรมทดสอบความอ้วนผอม");
+            clearForm();
+        }
+
+        private void btnBMI_Click(object sender, EventArgs e)
+        {
+            //ปุ่ม "คำนวณ BMI"
+            //in put รับค่า textbox
+            string name = txtName.Text;
+            int age = Convert.ToInt32(txtAge.Text);
+            //1 double height = Convert.ToDouble(txtHeight.Text);
+            //2 double height = 0;
+            //2 if (double.TryParse(txtHeight.Text, out height)==false)
+            //2 {
+            //2  MessageBox.Show("กรอกข้อมูลไม่ถูกต้อง", "เกิดข้อผิดพลาด");
+            //2    return; //ถ้าค่าใน if เป็นจริง ให้จบการทำงาน
+            //2 }
+            //1 double weight = Convert.ToDouble(txtWeight.Text);
+            //2 double weight = 0;
+            //2 if (double.TryParse(txtWeight.Text, out weight) == false)
+            //2 {
+            //2   MessageBox.Show("กรอกข้อมูลไม่ถูกต้อง", "เกิดข้อผิดพลาด");
+            //2    return; //ถ้าค่าใน if เป็นจริง ให้จบการทำงาน
+            //2 }
+            double height = 0;
+            double weight = 0;
+            if(CheckDouble(txtWeight,txtHeight))
+            {
+                height = Convert.ToDouble(txtWeight.Text);
+                weight = Convert.ToDouble(txtWeight.Text);
+            }
+            //Process คำนวณแล้ว BMi
+            //double bmi = weight / Math.Pow(height / 100, 2);
+            double bmi = BMI(height, weight);  //เรียกใช้ Method BMI
+            //Output แปลผล โดยนำค่า BMI มาเทียบกับค่ามาตรฐาน
+            lblResult.Text = bmi.ToString();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void clearForm()
+        {
+            txtAge.TextAlign = HorizontalAlignment.Right;  //จัดชิดขวา
+            txtHeight.TextAlign = HorizontalAlignment.Right;
+            txtWeight.TextAlign = HorizontalAlignment.Right;
+            txtName.Text = "";
+            txtAge.Text = "0";
+            txtHeight.Text = "0";
+            txtWeight.Text = "0";
+            txtName.Focus();
+
+        }
+        private void IntputDataValid(TextBox name, TextBox age) //ทดสอบ TextBox ใน Method
+        {
+            if (name.Text.Length != 0)      //.length ความยาวของ string
+            {
+                name.ForeColor = Color.DarkGreen;
+            }
+            else
+            {
+                name.ForeColor = Color.Black;
+            }
+            if (Convert.ToInt32(age.Text) > 0)
+            {
+                age.ForeColor = Color.DarkGreen;
+            }
+            else
+            {
+                age.ForeColor = Color.Black;
+            }
+        }
+
+        private void txtName_TextChanged(object sender, EventArgs e)
+        {
+            IntputDataValid(txtName, txtAge);
+        }
+
+        private void txtAge_TextChanged(object sender, EventArgs e)
+        {
+            IntputDataValid(txtName, txtAge);
+        }
+        private double BMI(double h,double w)  //คำนวณ bmi ส่งค่าเข้า 2 ค่าคือน้ำหนักและส่วนสูง
+        {
+           double bmi = w/Math.Pow(h/100,2);
+           return bmi; 
+           //return w / Math.Pow(h/100, 2);
+        }
+        private bool CheckDouble(TextBox txtW, TextBox txtH)  
+        {
+            double w = 0;
+            double h = 0;
+            if ((double.TryParse(txtW.Text, out w) == false) || (double.TryParse(txtW.Text, out h) == false))
+            {
+                MessageBox.Show("กรอกข้อมูลไม่ถูกต้อง", "เกิดข้อผิดพลาด");
+                return false;
+            }
+            return true;
+        }
+    }
+}
+
---
+++ WinFormsMethod/Form2.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace WinFormsMethod
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            txtResult = new TextBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(471, 12);
+            button1.Name = "button1";
+            button1.Size = new Size(177, 89);
+            button1.TabIndex = 0;
+            button1.Text = "GO!!!!";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // txtResult
+            // 
+            txtResult.Location = new Point(12, 12);
+            txtResult.Multiline = true;
+            txtResult.Name = "txtResult";
+            txtResult.Size = new Size(323, 501);
+            txtResult.TabIndex = 1;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.SkyBlue;
+            ClientSize = new Size(740, 576);
+            Controls.Add(txtResult);
+            Controls.Add(button1);
+            Name = "Form2";
+            Text = "Form2";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private TextBox txtResult;
+    }
+}
